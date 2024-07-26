8a1b998965a642c512a3c23e43621d3d37c7449f
gvenegas <gvenegas@sionpower.com>
2024-07-26 10:08:17 -0700

    bare

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 """
 File: main.py
 Author: Gabe Venegas
-Purpose: """
+Purpose: CLI based, extensible serial hardware logging and scripting tool.
+         Designed for use with Sion Power Corporation's
+         Voltage Sense & Balancing (VSB) boards."""
+         
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
